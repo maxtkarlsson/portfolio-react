c6a6e8132092016e0010dd31ca408865af4ceef1
maxtkarlsson <113439377+maxtkarlsson@users.noreply.github.com>
2023-05-11 09:20:53 +0200

    (feat): Add montserrat font

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,13 +1,14 @@
 import { Outlet } from "react-router";
 import { Navbar } from "./Navbar";
+import "./Layout.scss";
 
 export const Layout = () => {
   return (
     <>
-      <header>
+      <header className="header-container">
         <Navbar></Navbar>
       </header>
-      <main>
+      <main className="main-container">
         <Outlet></Outlet>
       </main>
       <footer></footer>
