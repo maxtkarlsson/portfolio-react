3257d4e1d9e185e4c487272a34522f856c75b2b0
maxtkarlsson <113439377+maxtkarlsson@users.noreply.github.com>
2023-05-11 21:36:51 +0200

    (feat): Add content and styling to About

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,12 +1,18 @@
 import { PageTitle } from "./PageTitle";
+import "./About.scss";
 
 export const About = () => {
   return (
-    <>
+    <div className="main-container__left--middle">
       <PageTitle
         title="MAX KARLSSON"
         desc="Front End Developer student"
       ></PageTitle>
-    </>
+      <p className="about-text">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam.
+      </p>
+    </div>
   );
 };
