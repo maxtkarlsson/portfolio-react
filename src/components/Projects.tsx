39cd6cf3ecb6a90adfafd86861569b4489ad75b8
maxtkarlsson <113439377+maxtkarlsson@users.noreply.github.com>
2023-05-11 21:37:46 +0200

    (feat): Add container classes and styling

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -2,8 +2,8 @@ import { PageTitle } from "./PageTitle";
 
 export const Projects = () => {
   return (
-    <>
+    <div className="main-container__left">
       <PageTitle title="Projects" desc=""></PageTitle>
-    </>
+    </div>
   );
 };
