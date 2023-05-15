906f53cde2a2988fd6b700380a833aa698ba7e33
maxtkarlsson <113439377+maxtkarlsson@users.noreply.github.com>
2023-05-15 09:20:32 +0200

    (fix): Add styling to About page

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,15 +4,17 @@ import "./About.scss";
 export const About = () => {
   return (
     <div className="main-container__left--middle">
-      <FrontPageTitle
-        title="MAX KARLSSON"
-        desc="Front End Developer student"
-      ></FrontPageTitle>
-      <p className="about-text">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam.
-      </p>
+      <div className="about-container">
+        <FrontPageTitle
+          name="MAX KARLSSON"
+          desc="Front End Developer student"
+        ></FrontPageTitle>
+        <p className="about-text">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam.
+        </p>
+      </div>
     </div>
   );
 };
--- src/components/FrontPageTitle.tsx
+++ src/components/FrontPageTitle.tsx
@@ -1,14 +1,14 @@
 import "./FrontPageTitle.scss";
 
 interface IFrontPageTitleProps {
-  title: string;
+  name: string;
   desc: string;
 }
 
-export const FrontPageTitle = ({ title, desc }: IFrontPageTitleProps) => {
+export const FrontPageTitle = ({ name, desc }: IFrontPageTitleProps) => {
   return (
     <div className="frontpage-title">
-      <h1 className="frontpage-title__title">{title}</h1>
+      <h1 className="frontpage-title__name">{name}</h1>
       <h3 className="frontpage-title__desc">{desc}</h3>
     </div>
   );
