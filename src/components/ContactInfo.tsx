6c4cfe40021249164671dbe998016870aa8f6588
maxtkarlsson <113439377+maxtkarlsson@users.noreply.github.com>
2023-05-16 14:08:31 +0200

    (feat): Add links to ContactInfo

--- src/components/ContactInfo.tsx
+++ src/components/ContactInfo.tsx
@@ -5,14 +5,24 @@ export const ContactInfo = () => {
     <div className="contact-info">
       <p className="contact-info__hello">Contact me!</p>
       <h4 className="contact-info__header">EMAIL</h4>
-      <p className="contact-info__text">max.karlsson@medieinstitutet.se</p>
+      <a
+        className="contact-info__text"
+        href="mailto:max.karlsson@medieinstitutet.se"
+      >
+        max.karlsson@medieinstitutet.se
+      </a>
       <h4 className="contact-info__header">TELEPHONE</h4>
-      <p className="contact-info__text">+46 73 967 57 59</p>
+      <a className="contact-info__text" href="tel:+46739675759">
+        +46 73 967 57 59
+      </a>
       <h4 className="contact-info__header">OTHER</h4>
       <div className="contact-info-icons">
-        <i className="bi bi-linkedin"></i>
-
-        <i className="bi bi-github"></i>
+        <a href="https://www.linkedin.com/in/max-karlsson-5307b1a3/">
+          <i className="bi bi-linkedin"></i>
+        </a>
+        <a href="https://github.com/maxtkarlsson">
+          <i className="bi bi-github"></i>
+        </a>
       </div>
     </div>
   );
