f7d1dbc2042a1a30a3590a4e4f31ab3879690023
maxtkarlsson <113439377+maxtkarlsson@users.noreply.github.com>
2023-05-22 11:18:18 +0200

    (refactor): Replace h3 tag with p tag

--- src/components/FrontPageTitle.tsx
+++ src/components/FrontPageTitle.tsx
@@ -9,7 +9,7 @@ export const FrontPageTitle = ({ name, desc }: IFrontPageTitleProps) => {
   return (
     <div className="frontpage-title">
       <h1 className="frontpage-title__name">{name}</h1>
-      <h3 className="frontpage-title__desc">{desc}</h3>
+      <p className="frontpage-title__desc">{desc}</p>
     </div>
   );
 };
