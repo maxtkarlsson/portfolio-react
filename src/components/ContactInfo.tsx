42926fd06695055c99cf1efefd05b95002ab2149
maxtkarlsson <113439377+maxtkarlsson@users.noreply.github.com>
2023-05-29 11:20:21 +0200

    (feat): Add StyledH4 to contact info

--- src/components/ContactInfo.tsx
+++ src/components/ContactInfo.tsx
@@ -1,21 +1,23 @@
 import "./ContactInfo.scss";
+import { StyledH4 } from "./styled/StyledH4";
 
 export const ContactInfo = () => {
   return (
     <div className="contact-info">
       <p className="contact-info__hello">Contact me!</p>
-      <h4 className="contact-info__header">EMAIL</h4>
+
+      <StyledH4>EMAIL</StyledH4>
       <a
         className="contact-info__text"
         href="mailto:max.karlsson@medieinstitutet.se"
       >
         max.karlsson@medieinstitutet.se
       </a>
-      <h4 className="contact-info__header">TELEPHONE</h4>
+      <StyledH4>TELEPHONE</StyledH4>
       <a className="contact-info__text" href="tel:+46739675759">
         +46 73 967 57 59
       </a>
-      <h4 className="contact-info__header">OTHER</h4>
+      <StyledH4>OTHER</StyledH4>
       <div className="contact-info-icons">
         <a href="https://www.linkedin.com/in/max-karlsson-5307b1a3/">
           <i className="bi bi-linkedin"></i>
--- src/components/styled/StyledH4.tsx
+++ src/components/styled/StyledH4.tsx
@@ -1,3 +1,5 @@
 import styled from "styled-components";
 
-export const StyledH4 = styled.h4``;
+export const StyledH4 = styled.h4`
+  margin-bottom: 0.3rem;
+`;
