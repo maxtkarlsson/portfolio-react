d92cb7d8f08b5767e190cc08e6148658a7efd4c6
maxtkarlsson <113439377+maxtkarlsson@users.noreply.github.com>
2023-05-29 16:17:29 +0200

    (refactor): Move resets to GlobalStyles component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { Layout } from "./components/Layout";
+import GlobalStyles from "./components/styled/Global";
 
 export const App = () => {
   return (
     <>
+      <GlobalStyles />
       <Layout></Layout>
     </>
   );
