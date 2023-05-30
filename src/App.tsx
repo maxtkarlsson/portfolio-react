6f16eb78023b81a22e182052e3b96edcd14201dc
maxtkarlsson <113439377+maxtkarlsson@users.noreply.github.com>
2023-05-30 10:11:29 +0200

    (feat): Add global style and theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import { Layout } from "./components/Layout";
 import GlobalStyles from "./components/styled/Global";
+import { ThemeProvider } from "styled-components";
+
+const theme = {
+  colors: {
+    morange: "#dd6e42",
+    mgrey: "#f5efed",
+    mblack: "#000000",
+    bkgblue: "#001d3d",
+  },
+};
 
 export const App = () => {
   return (
-    <>
-      <GlobalStyles />
-      <Layout></Layout>
-    </>
+    <ThemeProvider theme={theme}>
+      <>
+        <GlobalStyles />
+        <Layout></Layout>
+      </>
+    </ThemeProvider>
   );
 };
--- src/components/styled/Headings.tsx
+++ src/components/styled/Headings.tsx
@@ -1,22 +1,25 @@
 import styled from "styled-components";
 
 export const H1 = styled.h1`
+  color: ${({ theme }) => theme.colors.morange};
   font-weight: normal;
   font-size: 3rem;
 `;
 
 export const H2 = styled.h2`
-  color: #dd6e42;
+  color: ${({ theme }) => theme.colors.morange};
   font-weight: normal;
   font-size: 2rem;
 `;
 
 export const H3 = styled.h3`
+  color: ${({ theme }) => theme.colors.mblack};
   font-weight: normal;
   font-size: 1.6rem;
   margin-bottom: 1rem;
 `;
 
 export const H4 = styled.h4`
+  color: ${({ theme }) => theme.colors.mblack};
   margin-bottom: 0.3rem;
 `;
