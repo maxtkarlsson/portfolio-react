ffb6cae6f26a8457ec62ff6c9f2bfdd2b2e14c81
maxtkarlsson <113439377+maxtkarlsson@users.noreply.github.com>
2023-09-04 13:07:14 +0200

    (feat): Add temp filler container

--- src/components/styled/Wrappers.tsx
+++ src/components/styled/Wrappers.tsx
@@ -1,5 +1,16 @@
 import styled from "styled-components";
 
+/*TEMP FILLERS*/
+
+export const TempNav = styled.div`
+  background-color: coral;
+  border: 1px solid black;
+  height: 100px;
+  width: 100px;
+`;
+
+/*------------------------------ */
+
 export const PageWrapper = styled.div`
   display: flex;
   flex-direction: column;
@@ -12,6 +23,20 @@ export const PageWrapper = styled.div`
   }
   //flex-wrap: wrap;
   // skicka in size % och färg som props.
+`;
+
+export const HeaderWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  @media screen and (min-width: 768px) {
+    flex-direction: row;
+    justify-content: left;
+    height: 100%;
+    width: 41%;
+  }
 `;
 
 export const Sidebar = styled.div`
